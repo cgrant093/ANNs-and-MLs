b6c66ee459f139f813c0beea3532247f79679773
Cody M. Grant <cgrant093@gmail.com>
2021-09-22 15:01:29 -0400

    starting with simple NN, feed forward, back prop, and will expand on in the future

---
+++ Simple NN/main.py
@@ -0,0 +1,23 @@
+# neural network class definition
+class neuralNetwork:
+
+    #initialize the NN
+    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
+        
+        #set number of nodes in each layer
+        self.inodes = input_nodes
+        self.hnodes = hidden_nodes
+        self.onodes = output_nodes
+        
+        #set learning rate
+        self.lr = learning_rate
+        
+        pass
+        
+    #train the NN
+    def train():
+        pass
+        
+    #query the NN
+    def query():
+        pass
